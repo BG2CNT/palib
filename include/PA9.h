887d28947f80dca746d216f33ea965fd64729040
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-07-11 01:02:31 +0100

    docs: Update version number to 20250711

--- include/PA9.h
+++ include/PA9.h
@@ -2,7 +2,7 @@
 #define _PA_MAIN
 
 /*!
-	\mainpage PAlib 250228 Documentation
+	\mainpage PAlib 250711 Documentation
 
 	\section intro Introduction
 	Welcome to the PAlib documentation. Here you'll find
